b96465ab35d302fc72840da48861d125e5e55997
pabloapausa <pabloapausa@outlook.com>
2021-12-10 16:16:04 +0100

    Create Bottom update logic

--- frontend/src/components/atoms/ButtonUpdate/ButtonUpdate.jsx
+++ frontend/src/components/atoms/ButtonUpdate/ButtonUpdate.jsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import bankCreator from '../../../redux/actions/bankCreator';
 
 const ButtonUpdate = () => {
   const dispatch = useDispatch();
-  let isUpdated = false;
+  const [isUpdated, setIsUpdated] = useState(0);
 
-  const toggleValue = () => {
-    isUpdated = isUpdated !== true;
-  };
-  useEffect(() => {
-    dispatch(bankCreator());
-  }, isUpdated);
+  useEffect(() => { dispatch(bankCreator()); }, [isUpdated]);
   return (
     <button
       type="button"
-      onClick={toggleValue}
+      onClick={() => { setIsUpdated(isUpdated + 1); }}
     >
       Update
     </button>
--- frontend/src/redux/actions/bankCreator.js
+++ frontend/src/redux/actions/bankCreator.js
@@ -4,6 +4,7 @@ import actionTypes from './actionTypes';
 export default function bankCreator() {
   return async (dispatch) => {
     const { data } = await axios.get('https://restcountries.com/v3.1/all');
+    console.log('Mahia');
     dispatch({
       type: actionTypes.RENDER,
       data
