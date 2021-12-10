d61d495e4b8e2dfc588bb3fb3d4b87a29d0026ee
pabloapausa <pabloapausa@outlook.com>
2021-12-10 11:42:25 +0100

    Develop Redux

--- frontend/src/redux/reducers/checkoutReducer.js
+++ frontend/src/redux/reducers/checkoutReducer.js
@@ -1,9 +1,9 @@
 import actions from '../actions/actionTypes';
 
 export default function Reducer(initial = [], action) {
-  const render = initial;
+  let render = initial;
   if (action.type === actions.RENDER) {
-    checkout = action.data;
+    render = action.data;
   }
   return render;
 }
