68b27386b272b58a1d1d3e26ed0486cbb53d1c6c
pabloapausa <pabloapausa@outlook.com>
2021-12-10 18:28:56 +0100

    fix

--- frontend/src/components/atoms/DisplayRequests/DisplayRequests.jsx
+++ frontend/src/components/atoms/DisplayRequests/DisplayRequests.jsx
@@ -10,7 +10,7 @@ const DisplayRequests = () => {
   return (
     <div className="container">
       <p className="text">Accepted requests</p>
-      <p className="number">7 requests</p>
+      <p className="number">404278 requests</p>
     </div>
   );
 };
